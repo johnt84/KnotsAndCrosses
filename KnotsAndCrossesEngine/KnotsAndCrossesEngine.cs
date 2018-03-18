7cb876ad21b4797e95043dfdcaffd7c5b6aba0eb
johnt84 <johnt84@hotmail.co.uk>
2018-03-18 03:08:30 +0000

    Added Asp.Net page and game engine
    WIP
    
    Fixed problem with computer win logic
    
    Added winning line display
    
    Added winning line again
    
    Improved gui using css

---
+++ KnotsAndCrossesEngine/KnotsAndCrossesEngine.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Linq;
+
+namespace KnotsAndCrossesEngine
+{
+    public enum WinningMove
+    {
+        Row1,
+        Row2,
+        Row3,
+        Col1,
+        Col2,
+        Col3,
+        Diag1,
+        Diag2
+    }
+
+    [Serializable]
+    public class KnotsAndCrossesEngine
+    {
+        public int TurnCount { get; set; }
+        public int PlayerSwitch { get; set; }
+        public int NextPlayerSwitch { get; set; } 
+        public bool GameComplete { get; set; }
+        public int WinningPlayer { get; set; }
+        public static bool TwoPlayerMode { get; set; } = false;
+        public string[] GameBoard { get; set; }
+        public static string[] Player { get; set; } = { "X", "O" };
+
+    public KnotsAndCrossesEngine()
+        {
+            TurnCount = 1;
+            PlayerSwitch = 0;
+            NextPlayerSwitch = 0;
+            GameComplete = false;
+            WinningPlayer = 0;
+            GameBoard = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+        }
+
+        public string PlayerMove(int gameInput)
+        {
+            bool gamePositionAlreadyTaken = GameBoard[gameInput] != gameInput.ToString();
+            bool invalidGameInput = gameInput >= 9;
+
+            if (invalidGameInput || gamePositionAlreadyTaken)
+            {
+                if (invalidGameInput)
+                {
+                    return "Invalid position on the game board.Position must be between 0 and 8.";
+                }
+                else
+                {
+                    return $"\nPosition { gameInput } is already taken.";
+                }
+            }
+
+            PlayerSwitch = NextPlayerSwitch;
+            string knotOrCross = Player[PlayerSwitch];
+            GameBoard[gameInput] = Player[PlayerSwitch];
+
+            SetNextPlayer(PlayerSwitch);
+
+            return knotOrCross;
+        }
+
+        public MoveWinsGameCheck MoveWinsGame()
+        {
+            var checkMoveWinsGame = CheckMoveWinsGame();
+
+            GameComplete = checkMoveWinsGame.MoveWinsGame;
+
+            return checkMoveWinsGame;
+        }
+
+        private MoveWinsGameCheck CheckMoveWinsGame()
+        {
+            WinningMove winningMovePos = WinningMove.Row1;
+
+            bool horizontalCheck = false;
+
+            if (GameBoard[0] == GameBoard[1] && GameBoard[1] == GameBoard[2])
+            {
+                winningMovePos = WinningMove.Row1;
+                horizontalCheck = true;
+            }
+            else if (GameBoard[3] == GameBoard[4] && GameBoard[4] == GameBoard[5])
+            {
+                winningMovePos = WinningMove.Row2;
+                horizontalCheck = true;
+            }
+            else if (GameBoard[6] == GameBoard[7] && GameBoard[7] == GameBoard[8])
+            {
+                winningMovePos = WinningMove.Row3;
+                horizontalCheck = true;
+            }
+
+            bool verticalCheck = false;
+
+            if (GameBoard[0] == GameBoard[3] && GameBoard[3] == GameBoard[6])
+            {
+                winningMovePos = WinningMove.Col1;
+                verticalCheck = true;
+            }
+            else if (GameBoard[1] == GameBoard[4] && GameBoard[4] == GameBoard[7])
+            {
+                winningMovePos = WinningMove.Col2;
+                verticalCheck = true;
+            }
+            else if (GameBoard[2] == GameBoard[5] && GameBoard[5] == GameBoard[8])
+            {
+                winningMovePos = WinningMove.Col3;
+                verticalCheck = true;
+            }
+
+            bool diagonalCheck = false;
+
+            if (GameBoard[0] == GameBoard[4] && GameBoard[4] == GameBoard[8])
+            {
+                winningMovePos = WinningMove.Diag1;
+                diagonalCheck = true;
+            }
+            else if (GameBoard[2] == GameBoard[4] && GameBoard[4] == GameBoard[6])
+            {
+                winningMovePos = WinningMove.Diag2;
+                diagonalCheck = true;
+            }
+
+            string winningGameMessage = string.Empty;
+            bool moveWinsGame = false;
+
+            if(horizontalCheck || verticalCheck || diagonalCheck)
+            {
+                moveWinsGame = true;
+                WinningPlayer = PlayerSwitch + 1;
+                winningGameMessage = $"Congratulations!!  Player { WinningPlayer } wins!";
+            }
+
+            var moveWinsGameCheck = new MoveWinsGameCheck()
+            {
+                MoveWinsGame = moveWinsGame,
+                WinningMovePos = winningMovePos,
+                WinningGameMessage = winningGameMessage,
+            };
+
+            return moveWinsGameCheck;
+        }
+
+        public int CheckNextMoveWinsGame(string player)
+        {
+            int nextMove = -1;
+
+            if (GameBoard[0] == GameBoard[1] && GameBoard[2] == "2" && GameBoard[0] == player)
+            {
+                nextMove = 2;
+            }
+            else if (GameBoard[1] == GameBoard[2] && GameBoard[0] == "0" && GameBoard[1] == player)
+            {
+                nextMove = 0;
+            }
+            else if (GameBoard[0] == GameBoard[2] && GameBoard[1] == "1" && GameBoard[0] == player)
+            {
+                nextMove = 1;
+            }
+            else if (GameBoard[3] == GameBoard[4] && GameBoard[5] == "5" && GameBoard[3] == player)
+            {
+                nextMove = 5;
+            }
+            else if (GameBoard[3] == GameBoard[5] && GameBoard[4] == "4" && GameBoard[3] == player)
+            {
+                nextMove = 4;
+            }
+            else if (GameBoard[4] == GameBoard[5] && GameBoard[3] == "3" && GameBoard[4] == player)
+            {
+                nextMove = 3;
+            }
+            else if (GameBoard[6] == GameBoard[7] && GameBoard[8] == "8" && GameBoard[6] == player)
+            {
+                nextMove = 8;
+            }
+            else if (GameBoard[6] == GameBoard[8] && GameBoard[7] == "7" && GameBoard[6] == player)
+            {
+                nextMove = 7;
+            }
+            else if (GameBoard[7] == GameBoard[8] && GameBoard[6] == "6" && GameBoard[7] == player)
+            {
+                nextMove = 6;
+            }
+            else if (GameBoard[0] == GameBoard[3] && GameBoard[6] == "6" && GameBoard[0] == player)
+            {
+                nextMove = 6;
+            }
+            else if (GameBoard[0] == GameBoard[6] && GameBoard[3] == "3" && GameBoard[0] == player)
+            {
+                nextMove = 3;
+            }
+            else if (GameBoard[3] == GameBoard[6] && GameBoard[0] == "0" && GameBoard[3] == player)
+            {
+                nextMove = 0;
+            }
+            else if (GameBoard[1] == GameBoard[4] && GameBoard[7] == "7" && GameBoard[1] == player)
+            {
+                nextMove = 7;
+            }
+            else if (GameBoard[1] == GameBoard[7] && GameBoard[4] == "4" && GameBoard[1] == player)
+            {
+                nextMove = 4;
+            }
+            else if (GameBoard[4] == GameBoard[7] && GameBoard[1] == "1" && GameBoard[4] == player)
+            {
+                nextMove = 1;
+            }
+            else if (GameBoard[2] == GameBoard[5] && GameBoard[8] == "8" && GameBoard[2] == player)
+            {
+                nextMove = 8;
+            }
+            else if (GameBoard[5] == GameBoard[8] && GameBoard[2] == "2" && GameBoard[5] == player)
+            {
+                nextMove = 2;
+            }
+            else if (GameBoard[2] == GameBoard[8] && GameBoard[5] == "5" && GameBoard[1] == player)
+            {
+                nextMove = 5;
+            }
+            else if (GameBoard[0] == GameBoard[4] && GameBoard[8] == "8" && GameBoard[0] == player)
+            {
+                nextMove = 8;
+            }
+            else if (GameBoard[4] == GameBoard[8] && GameBoard[0] == "0" && GameBoard[4] == player)
+            {
+                nextMove = 0;
+            }
+            else if (GameBoard[0] == GameBoard[8] && GameBoard[4] == "4" && GameBoard[0] == player)
+            {
+                nextMove = 4;
+            }
+            else if (GameBoard[2] == GameBoard[4] && GameBoard[6] == "6" && GameBoard[2] == player)
+            {
+                nextMove = 6;
+            }
+            else if (GameBoard[2] == GameBoard[6] && GameBoard[4] == "4" && GameBoard[2] == player)
+            {
+                nextMove = 4;
+            }
+            else if (GameBoard[4] == GameBoard[6] && GameBoard[2] == "2" && GameBoard[6] == player)
+            {
+                nextMove = 2;
+            }
+
+            return nextMove;
+        }
+
+        public CompuerUserMove ComputerUserNextMove()
+        {
+            int computerNextMove = 0;
+
+            PlayerSwitch = NextPlayerSwitch;
+            SetNextPlayer(PlayerSwitch);
+
+            int moveFromOWinsGame = CheckNextMoveWinsGame("O");
+            int moveFromXWinsGame = CheckNextMoveWinsGame("X");
+
+            if (moveFromOWinsGame > -1)
+            {
+                computerNextMove = moveFromOWinsGame;
+            }
+            else if (moveFromXWinsGame > -1)
+            {
+                computerNextMove = moveFromXWinsGame;
+            }
+            else
+            {
+                var availablePositions = GameBoard.ToList()
+                      .Where(x => x != "O" && x != "X")
+                      .ToList();
+
+                var availablePositionArr = availablePositions.ToArray();
+
+                Random rnd = new Random();
+                int computerEntryPostion = rnd.Next(0, availablePositions.Count);
+                computerNextMove = Convert.ToInt32(availablePositionArr[computerEntryPostion]);
+            }
+
+            GameBoard[computerNextMove] = Player[PlayerSwitch];
+
+            var computerUserNextMove = new CompuerUserMove()
+            {
+                ComputerUserMovePos = computerNextMove,
+                ComputerUserMove = Player[PlayerSwitch],
+            };
+
+            return computerUserNextMove;
+        }
+
+        public void SetNextPlayer(int currentPlayer)
+        {
+            NextPlayerSwitch = (currentPlayer == 0) ? 1 : 0;
+        }
+    }
+
+    public class CompuerUserMove
+    {
+        public int ComputerUserMovePos { get; set; }
+        public string ComputerUserMove { get; set; }
+    }
+
+    public class MoveWinsGameCheck
+    {
+        public bool MoveWinsGame { get; set; }
+        public WinningMove WinningMovePos { get; set; }
+        public string WinningGameMessage { get; set; }
+    }
+}
---
+++ KnotsAndCrossesWebApp/Default.aspx.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using KnotsAndCrossesEngine;
+
+namespace KnotsAndCrossesWebApp
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        const string KNOTS_AND_CROSSES_ENGINE_NAME = "KnotsAndCrossesEngine";
+        string[] PlayerTurnMessage = new string[] { "Good move", "Ooh controversial", "Wow, didn't see that one coming", "Nice one", "Wow, this is tense", "Where did you pull that one out of", "Your on-fire" };
+
+        public KnotsAndCrossesEngine.KnotsAndCrossesEngine GameEngine
+        {
+            get
+            {
+                if (this.ViewState[KNOTS_AND_CROSSES_ENGINE_NAME] == null)
+                    return null;
+
+                return (KnotsAndCrossesEngine.KnotsAndCrossesEngine)this.ViewState[KNOTS_AND_CROSSES_ENGINE_NAME];
+            }
+            set { this.ViewState[KNOTS_AND_CROSSES_ENGINE_NAME] = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                NewGame();
+            }
+            else
+            {
+                if (ViewState[KNOTS_AND_CROSSES_ENGINE_NAME] != null)
+                {
+                    GameEngine = (KnotsAndCrossesEngine.KnotsAndCrossesEngine)ViewState[KNOTS_AND_CROSSES_ENGINE_NAME];
+                }
+            }
+        }
+
+        protected void btnNewGame_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        protected void btnPos0_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(0, btnPos0);
+        }
+
+        protected void btnPos1_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(1, btnPos1);
+        }
+
+        protected void btnPos2_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(2, btnPos2);
+        }
+
+        protected void btnPos3_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(3, btnPos3);
+        }
+
+        protected void btnPos4_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(4, btnPos4);
+        }
+
+        protected void btnPos5_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(5, btnPos5);
+        }
+
+        protected void btnPos6_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(6, btnPos6);
+        }
+
+        protected void btnPos7_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(7, btnPos7);
+        }
+
+        protected void btnPos8_Click(object sender, EventArgs e)
+        {
+            MakePlayerMove(8, btnPos8);
+        }
+
+        protected void MakePlayerMove(int gameBoardPos, Button btnPos)
+        {
+            string playerTurn = GameEngine.PlayerMove(gameBoardPos);
+
+            if (KnotsAndCrossesEngine.KnotsAndCrossesEngine.Player.Any(x => x == playerTurn))
+            {
+                btnPos.Text = playerTurn;
+                btnPos.ForeColor = Color.Blue;
+                btnPos.Enabled = false;
+                lblErrorMsg.Visible = false;
+
+                var moveWinsGameCheck = GameEngine.MoveWinsGame();
+
+                if(moveWinsGameCheck.MoveWinsGame)
+                {
+                    ToggleEnableAllButtons(false);
+                    lblGameStatusMsg.InnerText = moveWinsGameCheck.WinningGameMessage;
+                    DisplayWinningLine(moveWinsGameCheck.WinningMovePos);
+                }
+                else if(GameEngine.GameBoard.All(x => KnotsAndCrossesEngine.KnotsAndCrossesEngine.Player.Any(y => x == y)))
+                {
+                    ToggleEnableAllButtons(false);
+                    lblGameStatusMsg.InnerText = "Cats game, game ends in a draw!";
+                }
+                else
+                {
+                    bool player2IsUser = GameEngine.NextPlayerSwitch == 1 && KnotsAndCrossesEngine.KnotsAndCrossesEngine.TwoPlayerMode;
+                    bool player2IsComputer = GameEngine.NextPlayerSwitch == 1 && !KnotsAndCrossesEngine.KnotsAndCrossesEngine.TwoPlayerMode;
+
+                    if (GameEngine.NextPlayerSwitch == 0 || player2IsUser)
+                    {
+                        lblGameStatusMsg.InnerText = $"{ NextPlayerTurnMessage() }!  Player { GameEngine.NextPlayerSwitch + 1 } take your turn";
+                    }
+                    else
+                    {
+                        DisplayComputerNextMove(GameEngine.ComputerUserNextMove());
+
+                        var computerWinsGameCheck = GameEngine.MoveWinsGame();
+
+                        if (computerWinsGameCheck.MoveWinsGame)
+                        {
+                            ToggleEnableAllButtons(false);
+                            lblGameStatusMsg.InnerText = computerWinsGameCheck.WinningGameMessage;
+                            DisplayWinningLine(computerWinsGameCheck.WinningMovePos);
+                        }
+                        else if (GameEngine.GameBoard.All(x => KnotsAndCrossesEngine.KnotsAndCrossesEngine.Player.Any(y => x == y)))
+                        {
+                            ToggleEnableAllButtons(false);
+                            lblGameStatusMsg.InnerText = "Cats game, game ends in a draw!";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                lblErrorMsg.InnerText = playerTurn;
+                lblErrorMsg.Visible = true;
+            }
+        }
+
+        protected void NewGame()
+        {
+            GameEngine = new KnotsAndCrossesEngine.KnotsAndCrossesEngine();
+            ToggleEnableAllButtons(true);
+            ClearAllButtons();
+            ClearBoard();
+            lblErrorMsg.Visible = false;
+            lblGameStatusMsg.InnerText = $"NEW GAME.  Player { GameEngine.PlayerSwitch + 1 } take your turn";
+
+        }
+
+        protected void ToggleEnableAllButtons(bool isDisabled)
+        {
+            btnPos0.Enabled = isDisabled;
+            btnPos1.Enabled = isDisabled;
+            btnPos2.Enabled = isDisabled;
+            btnPos3.Enabled = isDisabled;
+            btnPos4.Enabled = isDisabled;
+            btnPos5.Enabled = isDisabled;
+            btnPos6.Enabled = isDisabled;
+            btnPos7.Enabled = isDisabled;
+            btnPos8.Enabled = isDisabled;
+        }
+
+        protected void ClearAllButtons()
+        {
+            btnPos0.Text = string.Empty;
+            btnPos1.Text = string.Empty;
+            btnPos2.Text = string.Empty;
+            btnPos3.Text = string.Empty;
+            btnPos4.Text = string.Empty;
+            btnPos5.Text = string.Empty;
+            btnPos6.Text = string.Empty;
+            btnPos7.Text = string.Empty;
+            btnPos8.Text = string.Empty;
+        }
+
+        private string NextPlayerTurnMessage()
+        {
+            Random rnd = new Random();
+            return PlayerTurnMessage[rnd.Next(0, PlayerTurnMessage.Length)];
+        }
+
+        private void DisplayComputerNextMove(CompuerUserMove computerUserMove)
+        {
+            switch (computerUserMove.ComputerUserMovePos)
+            {
+                case 0:
+                    btnPos0.Text = computerUserMove.ComputerUserMove;
+                    btnPos0.ForeColor = Color.Red;
+                    btnPos0.Enabled = false;
+                    break;
+                case 1:
+                    btnPos1.Text = computerUserMove.ComputerUserMove;
+                    btnPos1.ForeColor = Color.Red;
+                    btnPos1.Enabled = false;
+                    break;
+                case 2:
+                    btnPos2.Text = computerUserMove.ComputerUserMove;
+                    btnPos2.ForeColor = Color.Red;
+                    btnPos2.Enabled = false;
+                    break;
+                case 3:
+                    btnPos3.Text = computerUserMove.ComputerUserMove;
+                    btnPos3.ForeColor = Color.Red;
+                    btnPos3.Enabled = false;
+                    break;
+                case 4:
+                    btnPos4.Text = computerUserMove.ComputerUserMove;
+                    btnPos4.ForeColor = Color.Red;
+                    btnPos4.Enabled = false;
+                    break;
+                case 5:
+                    btnPos5.Text = computerUserMove.ComputerUserMove;
+                    btnPos5.ForeColor = Color.Red;
+                    btnPos5.Enabled = false;
+                    break;
+                case 6:
+                    btnPos6.Text = computerUserMove.ComputerUserMove;
+                    btnPos6.ForeColor = Color.Red;
+                    btnPos6.Enabled = false;
+                    break;
+                case 7:
+                    btnPos7.Text = computerUserMove.ComputerUserMove;
+                    btnPos7.ForeColor = Color.Red;
+                    btnPos7.Enabled = false;
+                    break;
+                case 8:
+                    btnPos8.Text = computerUserMove.ComputerUserMove;
+                    btnPos8.ForeColor = Color.Red;
+                    btnPos8.Enabled = false;
+                    break;
+            }
+        }
+
+        private void ClearBoard()
+        {
+            btnPos0.BackColor = Color.White;
+            btnPos1.BackColor = Color.White;
+            btnPos2.BackColor = Color.White;
+            btnPos3.BackColor = Color.White;
+            btnPos4.BackColor = Color.White;
+            btnPos5.BackColor = Color.White;
+            btnPos6.BackColor = Color.White;
+            btnPos7.BackColor = Color.White;
+            btnPos8.BackColor = Color.White;
+        }
+
+        private void DisplayWinningLine(WinningMove winningMove)
+        {
+            switch (winningMove)
+            {
+                case WinningMove.Row1:
+                    btnPos0.BackColor = Color.Yellow;
+                    btnPos1.BackColor = Color.Yellow;
+                    btnPos2.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Row2:
+                    btnPos3.BackColor = Color.Yellow;
+                    btnPos4.BackColor = Color.Yellow;
+                    btnPos5.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Row3:
+                    btnPos6.BackColor = Color.Yellow;
+                    btnPos7.BackColor = Color.Yellow;
+                    btnPos8.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Col1:
+                    btnPos0.BackColor = Color.Yellow;
+                    btnPos3.BackColor = Color.Yellow;
+                    btnPos6.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Col2:
+                    btnPos1.BackColor = Color.Yellow;
+                    btnPos4.BackColor = Color.Yellow;
+                    btnPos7.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Col3:
+                    btnPos2.BackColor = Color.Yellow;
+                    btnPos5.BackColor = Color.Yellow;
+                    btnPos8.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Diag1:
+                    btnPos0.BackColor = Color.Yellow;
+                    btnPos4.BackColor = Color.Yellow;
+                    btnPos8.BackColor = Color.Yellow;
+                    break;
+                case WinningMove.Diag2:
+                    btnPos2.BackColor = Color.Yellow;
+                    btnPos4.BackColor = Color.Yellow;
+                    btnPos6.BackColor = Color.Yellow;
+                    break;
+            }
+        }
+
+    }
+}
