b51591768c3ecc6629e6485d0edf6103d90a2537
John Tomlinson <johnt84@hotmail.co.uk>
2021-01-04 13:46:27 +0000

    Migrated the Knots and Crosses Blazor Web Assembly project from .Net Core 3 to .Net 5

--- KnotsAndCrossesBlazorWasm/Program.cs
+++ KnotsAndCrossesBlazorWasm/Program.cs
@@ -1,12 +1,8 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace KnotsAndCrossesBlazorWasm
 {
@@ -15,9 +11,9 @@ namespace KnotsAndCrossesBlazorWasm
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddSingleton<KnotsAndCrossesEngine.KnotsAndCrossesEngine>();
 
