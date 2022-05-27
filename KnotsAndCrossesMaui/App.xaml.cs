ae598c1cc28c9052ac67d544777905651af0cf5f
John Tomlinson <jtomlinson@brightree.com>
2022-05-27 16:23:49 +0100

    Created .NET Blazor MAUI Knots & Crosses app

---
+++ KnotsAndCrossesMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KnotsAndCrossesMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
