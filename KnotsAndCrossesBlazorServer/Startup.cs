768674d54895b22f9aeb85e50b4f9463a81b7afd
John Tomlinson <jtomlinson@brightree.com>
2022-05-30 12:06:03 +0100

    Renamed KnotsAndCrossesBlazorApp project to KnotsAndCrossesBlazorServer and moved the Knots and Crosses engine back to .Net Stadnard v 2 so it can work with the Web Forms version of the game

--- KnotsAndCrossesBlazorServer/Startup.cs
+++ KnotsAndCrossesBlazorServer/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 
-namespace KnotsAndCrossesBlazorApp
+namespace KnotsAndCrossesBlazorServer
 {
     public class Startup
     {
